191cec55c00d13a8a7ebab3fda7bd6fb66e4e706
Terry <158463579+gpsftuEbsl@users.noreply.github.com>
2024-10-10 23:41:46 +0800

    Create f607.cpp

---
+++ f607.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long n, l, x, i, flag, head, ans = 0, cin_rank[200005];
+    set<int> cut_id;
+    cin >> n >> l;
+    cut_id.insert(0);
+    cut_id.insert(l);
+    for(int j = 0; j < n; j++){
+        cin >> x >> i;
+        cin_rank[i] = x;
+    }
+    for(int j = 1; j <= n; j++){
+        cut_id.insert(cin_rank[j]);
+        auto tail = cut_id.find(cin_rank[j]), head = tail;
+        head++; tail--;
+        ans += *(head)-*(tail);
+        //check0
+        //cout << "ans+=" << *(head)-*(tail) << endl;
+        //check1
+    }
+    cout << ans << endl;
+}
